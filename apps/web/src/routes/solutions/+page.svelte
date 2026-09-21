ef6d6412f4ce57c6a893b1ea3c8b590f72e70d8f
opensophy-projects <veilstack.it@gmail.com>
2026-09-21 21:12:59 +0700

    Updated +page.svelte

--- apps/web/src/routes/solutions/+page.svelte
+++ apps/web/src/routes/solutions/+page.svelte
@@ -30,7 +30,7 @@
     <ServicesGrid />
     <section class="mt-14 flex flex-wrap gap-3">
       <button class="action primary" onclick={() => contactsState.open()}
-        >Заказать услуги /></button
+        >Заказать услуги</button
       ><a class="action" href="/service-policy">Политика оказания услуг</a>
     </section>
   </div>
